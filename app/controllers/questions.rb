5667a0c7dee1d353b060927a488a9b2d1e0ea143
Lou Gaeta & Natalie Ruiz <pair+louisgaeta1.nataliecita@devbootcamp.com>
2017-05-18 16:14:07 -0400

    Add errors partial, show page, new form

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -4,13 +4,20 @@ get '/questions' do
 end
 
 get '/questions/new' do
-
+  erb :'/questions/new'
 end
 
 get '/questions/:id' do
-
+  @question = Question.find(params[:id])
+  erb :'/questions/show'
 end
 
 post '/questions' do
-
+  question = Question.new(params[:question])
+  if question.save
+    redirect '/questions'
+  else
+    @errors = question.errors.full_messages
+    erb :'/questions/new'
+  end
 end
