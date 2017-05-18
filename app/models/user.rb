7823ea4449a1d6f3016f5615cd0cb8c5fc64b600
Alexandra Hill & Lou Gaeta & Natalie Ruiz <pair+alexnhill.louisgaeta1.nataliecita@devbootcamp.com>
2017-05-18 15:36:02 -0400

    Completes seed data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true
   has_secure_password
   has_many :answers
-  has_many :created_questions, class_name: "Question"
+  has_many :created_questions, class_name: "Question", foreign_key: :author_id
   has_many :answered_questions, through: :answers, source: :question
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,12 @@
   User.create(email: Faker::Internet.email, password: "password")
 end
 
-# User.all.each do |user|
-#   3.times do
-#     user.questions << Question.create(title: )
-#   end
-# end
+User.all.each do |user|
+  3.times do
+    user.created_questions << Question.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+  end
+  3.times do
+    answer = Answer.new(body: Faker::Lorem.paragraph, user_id: user.id, question_id: rand(1..30))
+    answer.save!
+  end
+end
