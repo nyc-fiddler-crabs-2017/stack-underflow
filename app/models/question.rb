f19401eba7077788d2b653d60d86d6efc5b191ed
Lou Gaeta <email": null>
2017-05-19 14:55:41 -0400

    Creates Vote Model and migration, creates voteable module for models

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,10 @@ class Question < ActiveRecord::Base
   validates :title, :body, :author_id, presence: true
 
   belongs_to :author, class_name: "User"
-  # has_one :best_answer, class_name: "Answer"
   has_many :answers
   has_many :responders, through: :answers, source: :user
+  has_many :votes, as: :voteable
+  include Voteable
 
   def best_answer
     Answer.find_by_id(best_answer_id)
