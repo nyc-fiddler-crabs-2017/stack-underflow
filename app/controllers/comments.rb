96bc8c66d992d71538fadfdb373292b903f91a67
Natalie Ruiz <email": null>
2017-05-19 15:28:24 -0400

    create initial routes

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,7 +1,7 @@
-get 'questions/:question_id/comments/new' do
+get '/questions/:question_id/comments/new' do
   "this is the new comment get route"
 end
 
-post 'questions/:question_id/comments' do
+post '/questions/:question_id/comments' do
   "This is the new comment post route"
 end
