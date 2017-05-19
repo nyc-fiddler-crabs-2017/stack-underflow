d18fb3045dc507e4ef310d067df2aadd69bd5f34
Lou Gaeta <email": null>
2017-05-19 13:57:58 -0400

    Makes seed data more readable

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,11 @@ end
 
 User.all.each do |user|
   3.times do
-    user.created_questions << Question.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+    title = Faker::Hacker.adjective + " " + Faker::Hacker.noun
+    user.created_questions << Question.create(title: title, body: Faker::Lorem.paragraph)
   end
   3.times do
-    answer = Answer.new(body: Faker::Lorem.paragraph, user_id: user.id, question_id: rand(1..30))
+    answer = Answer.new(body: Faker::Hacker.say_something_smart, user_id: user.id, question_id: rand(1..30))
     answer.save!
   end
 end
