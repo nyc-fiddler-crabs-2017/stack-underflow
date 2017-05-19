a26d393bdb1cd0462900c3645b17583fbe10bc12
Natalie Ruiz <email": null>
2017-05-19 17:10:49 -0400

    Errors for commenting are working

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,15 +3,18 @@ end
 
 post '/questions/:question_id/comments' do
   verify
-  question = Question.find(params[:question_id])
+  @question = Question.find(params[:question_id])
 
-  @comment = question.comments.new(body: params[:comment][:body])
+  @comment = @question.comments.new(body: params[:comment][:body])
   current_user.comments << @comment
   if @comment.save
-    redirect "/questions/#{question.id}"
+    if request.xhr?
+      erb :'comments/_comment', layout: false, locals: {comment: @comment}
+    else
+    redirect "/questions/#{@question.id}"
+    end
   else
-    redirect "/"
-      # @errors = @comment.errors.full_messages
-      # erb :"/questions/#{question}"
+      @errors = @comment.errors.full_messages
+      erb :"/_errors", locals: {question: @question}, layout: false
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -30,4 +30,19 @@ $(document).ready(function() {
     });
   });
 
+  $(".new-comment-form").on("submit", function(event){
+    event.preventDefault();
+    var form = $(this);
+    var action = form.attr('action');
+    var data = form.serialize();
+    $.ajax({
+      method: form.attr("method"),
+      url: action,
+      data: data
+    }).done(function(response){
+      $(".comments-ul").append(response);
+      $('.new-comment-form').trigger('reset');
+    });
+  });
+
 });
