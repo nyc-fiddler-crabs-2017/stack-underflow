7191a0c7103c052c7108d1b5eb464fbfc53e9656
Natalie Ruiz <email": null>
2017-05-19 16:21:38 -0400

    Comment saves :)

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,7 +1,17 @@
 get '/questions/:question_id/comments/new' do
-  "this is the new comment get route"
 end
 
 post '/questions/:question_id/comments' do
-  "This is the new comment post route"
+  verify
+  question = Question.find(params[:question_id])
+
+  @comment = question.comments.new(body: params[:comment][:body])
+  current_user.comments << @comment
+  if @comment.save
+    redirect "/questions/#{question.id}"
+  else
+    redirect "/"
+      # @errors = @comment.errors.full_messages
+      # erb :"/questions/#{question}"
+  end
 end
